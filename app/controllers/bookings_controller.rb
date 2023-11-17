e953024014a72b115a5924007cf29a80e646c7fa
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-11-17 18:05:57 +0300

    intergrated user and  profile

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,33 +1,11 @@
-# class BookingsController < ApplicationController
-#   before_action :authenticate_user!, only: [:create]
-
-#   def create
-#     @booking = current_user.bookings.build(booking_params)
-
-#     if @booking.save
-#       render json: @booking, status: :created
-#     else
-#       render json: { errors: @booking.errors.full_messages }, status: :unprocessable_entity
-#     end
-#   end
-
-#   private
-
-#   def booking_params
-#     params.require(:booking).permit(
-#       :movingDate, :movingTime, :currentAddress, :destinationAddress,
-#       :propertyType, :bedrooms, :services, :crewSize,
-#       :vehicleSize, :instructions, :emergencyContactName,
-#       :emergencyContactPhoneNumber, :relationshipToUser,
-#       :mpesaNumber, :mpesaCode, :termsAccepted
-#     )
-#   end
-# end
-# app/controllers/bookings_controller.rb
-
 class BookingsController < ApplicationController
   before_action :authenticate_user!, only: [:create]
 
+  def index 
+    booking = Booking.all 
+    render json: booking
+  end
+  
   def create
      puts "Received params: #{params.inspect}"
     @booking = current_user.bookings.build(booking_params)
